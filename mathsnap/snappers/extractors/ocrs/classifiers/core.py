a911a2ff10c73c8012dc8960070f36b01b75f676
Tim Frey <tim@frey.uno>
2019-08-13 10:43:10 +0200

    Added KerasClassifier - loads model from file - sign number for image based on the loaded "Keras"-Model

--- mathsnap/snappers/extractors/ocrs/classifiers/core.py
+++ mathsnap/snappers/extractors/ocrs/classifiers/core.py
@@ -1,6 +1,8 @@
 from typing import NamedTuple
+from keras.models import load_model
 
 import numpy as np
+
 
 
 class ClassifierResult(NamedTuple):
@@ -17,3 +19,13 @@ class DummyClassifier(Classifier):
         return ClassifierResult(
             label='2'
         )
+
+
+# TODO : Define the image size
+class KerasClassifier(Classifier):
+    def __init__(self, file_name: str):
+        self.model = load_model(file_name)
+
+    def process(self, image: np.ndarray) -> ClassifierResult:
+        return ClassifierResult(label=self.model.perdict(image))
+
