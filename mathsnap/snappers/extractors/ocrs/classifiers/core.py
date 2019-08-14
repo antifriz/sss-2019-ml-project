fd2188aaa247a04021a5c94f47d0a1bc14f3bc68
Tim Frey <tim@frey.uno>
2019-08-14 16:18:11 +0200

    BugFix: padding, inverting and binarize

--- mathsnap/snappers/extractors/ocrs/classifiers/core.py
+++ mathsnap/snappers/extractors/ocrs/classifiers/core.py
@@ -32,8 +32,8 @@ class KerasClassifier(Classifier):
         model = load_model(self.file_name)
 
         img = cv2.resize(image, (20, 20))
+        img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 21, 10)
         img = cv2.copyMakeBorder(img, 4, 4, 4, 4, cv2.BORDER_CONSTANT, value=0)
-        img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 21, 0)
         img = img[np.newaxis, :, :, np.newaxis]
 
         prediction = list(model.predict(img, 1)[0]).index(1)
