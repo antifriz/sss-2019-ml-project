26b24819087a41f45ed4de84b9880c3fcaaa71f0
Sanja <sanjabgd01@gmail.com>
2019-08-14 15:21:36 +0200

    Padding image

--- mathsnap/snappers/extractors/ocrs/classifiers/core.py
+++ mathsnap/snappers/extractors/ocrs/classifiers/core.py
@@ -31,12 +31,12 @@ class KerasClassifier(Classifier):
     def process(self, image: np.ndarray) -> ClassifierResult:
         model = load_model(self.file_name)
 
-        img = cv2.resize(image, (28, 28))
+        img = cv2.resize(image, (20, 20))
+        img = cv2.copyMakeBorder(img, 4, 4, 4, 4, cv2.BORDER_CONSTANT, value=0)
         img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 21, 0)
-        img = np.expand_dims(img, axis=-1)
-        img = np.expand_dims(img, axis=0)
+        img = img[np.newaxis, :, :, np.newaxis]
 
-        prediction = list(model.predict(img, 1)[0]).index(1);
+        prediction = list(model.predict(img, 1)[0]).index(1)
 
         return ClassifierResult(label=str(prediction))
 
